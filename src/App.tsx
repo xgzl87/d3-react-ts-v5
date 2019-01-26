0f604601703ad295be9fa615c8b5fc2c353fcf82
lzhuang <324505520@qq.com>
2019-01-26 22:48:10 +0800

    create d3 project....

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,206 @@
+import * as d3 from "d3";
 import * as React from 'react';
-import './App.css';
+import Tree from 'react-d3-tree';
+import {getTreeJsonData, isSupportSVG} from "./utils/util";
 
-import logo from './logo.svg';
+// import {initParams} from "./interfaces/InitTreeParams"
+export interface IProps {
+    width: number;
+    height: number;
+    offsetTreeH: number;
+    nodeHeight: number;
+    nodeWidth?: number;
+    fontColor?: string;
+    pathColor?: string;
+}
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+interface IState {
+    width: number;
+    height: number;
+    duration: number;
+    margin: {
+        top: number,
+        bottom: number,
+        left: number,
+        right: number
+    };
+    zoom: any;
+    root: any;
+    nodes: any;
+    dTreeData: any;
+    links: any;
+}
+
+class App extends React.Component<any, IState> {
+    constructor(props: any) {
+        super(props);
+        this.getRoot = this.getRoot.bind(this);
+        // @ts-ignore
+        this.state = {
+            width: 960,
+            height: 600,
+            duration: 750,
+            margin: {
+                top: 30,
+                bottom: 0,
+                left: 80,
+                right: 0
+            },
+            zoom: null,
+            root: null,
+            nodes: [] as any,
+            dTreeData: [] as any,
+            links: [] as any
+        };
+    }
+
+    public componentDidMount() {
+        if (!isSupportSVG()) {
+            console.log("浏览器不支持svg,请升级!");
+            return;
+        }
+        // this.mounted();
+    }
+
+    public render() {
+        //    console.log(this.getRoot());
+        return (
+            <div id="treeWrapper" style={{width: this.state.width + "px", height: this.state.height + "px"}}>
+                <Tree data={getTreeJsonData()} separation={{siblings: 0.3, nonSiblings: 0.5}}/>
+            </div>
+        );
+    }
+
+    protected getRoot() {
+        const root = d3.hierarchy(getTreeJsonData())
+            .sum((d: any) => {
+                return d.value;
+            });
+        return root
+    }
+
+    protected getNodesAndLinks() {
+        // createTree generate new x、y coordinate according to root node,
+        this.setState({dTreeData: this.createTree()(this.state.root)});
+        this.setState({nodes: this.state.dTreeData.descendants(), links: this.state.dTreeData.descendants().slice(1)});
+    }
+
+    protected clickNode(d: any) {
+        if (!d._children && !d.children) {
+            return;
+        }
+
+        if (d.children) {
+            d = Object.assign({}, d, {_children: d.children});
+            // this.$set(d, '_children', d.children)
+            d.children = null
+        } else {
+            d = Object.assign({}, d, {children: d._children});
+            d._children = null
+        }
+        this.update(d);
+
+    }
+
+
+    protected update(source: any) {
+        // @ts-ignore
+        const svg = d3.select("svg");
+
+        const g = svg.append("g")
+            .attr("transform", "translate(" + this.state.margin.left + "," + this.state.margin.top + ")");
+
+        const hierarchyData = source;
+
+        // @ts-ignore
+        const tree = d3.tree()
+            .size([Number(this.state.width) - 400, Number(this.state.height) - 200])
+            .separation((a, b) => {
+                return (a.parent === b.parent ? 1 : 2) / a.depth;
+            });
+
+        const treeData = tree(hierarchyData);
+        const nodes = treeData.descendants();
+        const links = treeData.links();
+
+        // 输出节点和边
+
+        const bezierCurveGenerator = d3.linkHorizontal()
+            .x((d: any) => {
+                return d.y;
+            })
+            .y((d: any) => {
+                return d.x;
+            });
+
+        // 绘制边
+        g.append("g")
+            .selectAll("path")
+            .data(links)
+            .enter()
+            .append("path")
+            .attr("d", (d: any) => {
+                const start = {x: d.source.x, y: d.source.y};
+                const end = {x: d.target.x, y: d.target.y};
+                // @ts-ignore
+                return bezierCurveGenerator({source: start, target: end});
+            })
+            .attr("fill", "none")
+            .attr("stroke", "yellow")
+            .attr("stroke-width", 1);
+
+        const gs = g.append("g")
+            .selectAll("g")
+            .data(nodes)
+            .enter()
+            .append("g")
+            .attr("transform", (d: any) => {
+                const cx = d.x;
+                const cy = d.y;
+                return "translate(" + cy + "," + cx + ")";
+            });
+
+        // 绘制节点
+        gs.append("circle")
+            .attr("r", 6)
+            .attr("fill", "white")
+            .attr("stroke", "blue")
+            .attr('pointer-events', 'mouseover')
+            .attr("opacity", 0.2) // change this to zero to hide the target area
+            .attr("stroke-width", 1)
+            .on('click', (d) => {
+                this.clickNode(d);
+                console.log(d);
+            });
+
+        // 文字
+        gs.append("text")
+            .attr("x", (d: any) => {
+                if (d.data && d.data.nodeWidth && d.children) {
+                    return -d.data.nodeWidth;
+                } else {
+                    return d.children ? (d.data.name.length > 2 ? -60 : -40) : 8;
+                }
+            })
+            .attr("y", -5)
+            .attr("dy", 10)
+            .text((d: any) => {
+                return d.data && d.data.name;
+            });
+    }
+
+    protected mounted() {
+        // 创建svg画布
+        this.update(this.state.root);
+    }
+
+    protected createTree() {
+        return d3.tree()
+            .size([this.state.height, this.state.width])
+            .separation((a, b) => {
+                return (a.parent === b.parent ? 1 : 2) / a.depth;
+            });
+    }
 }
 
 export default App;
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,178 @@
+import * as $ from "jquery";
+import {IInitTreeParams} from "../interfaces/InitTreeParams";
 
-export const initParams = () => {
+declare let window: any;
+export const initParams = (option: IInitTreeParams) => {
+    const defaultOpt = {
+        width: 900,
+        height: 900,
+        offsetTreeH: 300,// 影响树形横向的区域
+        nodeHeight: 0,// 节点rect高度
+        nodeWidth: {// 传参为null就按字数设置宽度
+            _0: 90,
+            _1: 90,
+            _2: 90,
+            _3: 100,
+            _4: 90
+        },// 默认节点宽度
+        selector: null,// 挂载元素
+        fontNum: 1.2,// 设置字体大小因子
+        fontColors: {// 字体颜色
+            normal: '#000',
+            warning: '#e3e3e1',
+            errors: '#f00'
+        },
+        bgColors: {
+            normal: '#DCE6F2',
+            warning: '#e3e3e1',
+            errors: '#f00'
+        },
+        dataUrl: '' // 必填
+    };
+
+    option = $.extend(true, defaultOpt, option);
+
+    return option;
+};
+
+// 判断浏览器是否支持svg
+export const isSupportSVG = () => {
+    const svgNs = 'http://www.w3.org/2000/svg';
+    return !!window.document.createElementNS && !!window.document.createElementNS(svgNs, 'svg').createSVGRect;
+};
+
+// 获取树结构的数据
+export const getTreeJsonData = () => {
+    return {
+        "name": "思辨-储备",
+        "number": "12",
+        "type": "1",
+        "children":
+            [
+                {
+                    "name": "产品节奏",
+                    "number": "3",
+                    "value": 100,
+                    "type": "1",
+                    "children":
+                        [
+                            {
+                                "name": "2019 上半年",
+                                "nodeWidth": "80",
+                                "number": "381",
+                                "type": "1",
+                                "children":
+                                    [
+                                        {
+                                            "name": "Web 前端",
+                                            "number": "180",
+                                            "type": "1"
+                                        },
+                                        {
+                                            "name": "后端 (NodeJS)",
+                                            "number": "145",
+                                            "type": "1"
+                                        }
+                                    ]
+                            },
+                            {
+                                "name": "2019 下半年",
+                                "nodeWidth": "80",
+                                "number": "121",
+                                "type": "1",
+                                "children": [{
+                                    "name": "移动端开发人员",
+                                    "nodeWidth": "105",
+                                    "number": "1",
+                                    "type": "1",
+                                },
+                                    {
+                                        "name": "后端 (NodeJS)",
+                                        "number": "1",
+                                        "type": "1",
+                                    },
+                                    {
+                                        "name": "文档解析",
+                                        "number": "1",
+                                        "nodeWidth": "70",
+                                        "type": "1",
+                                    },
+                                    {
+                                        "name": "复杂方案设计过程中的智能支持",
+                                        "number": "1",
+                                        "nodeWidth": "190",
+                                        "type": "1",
+                                    }]
+                            }
+                        ]
+                },
+                {
+                    "name": "人员标准",
+                    "number": "1",
+                    "type": "1",
+                    "value": 100,
+                    "children":
+                        [
+                            {
+                                "name": "技术有特长",
+                                "number": "183",
+                                "type": "1",
+                                "value": 100
+                            },
+                            {
+                                "name": "能够在南京工作或持续出差",
+                                "nodeWidth": "190",
+                                "number": "8",
+                                "type": "1"
+                            }
+                        ]
+                },
+                {
+                    "name": "技术需求",
+                    "number": "1",
+                    "type": "1",
+                    "children":
+                        [
+                            {
+                                "name": "图例",
+                                "number": "183",
+                                "type": "1",
+                                "children":
+                                    [
+                                        {
+                                            "name": "需要的",
+                                            "number": "183",
+                                            "type": "1"
+                                        },
+                                        {
+                                            "name": "关注中, 可能需要的",
+                                            "nodeWidth": "120",
+                                            "number": "8",
+                                            "type": "1"
+                                        }
+                                    ]
+                            },
+                            {
+                                "name": "语言",
+                                "number": "183",
+                                "type": "1",
+                                "children":
+                                    [
+                                        {
+                                            "name": "JavaScript/TypeScript",
+                                            "number": "183",
+                                            "nodeWidth": "150",
+                                            "type": "1"
+                                        },
+                                        {
+                                            "name": "Python",
+                                            "number": "8",
+                                            "type": "1"
+                                        }
+                                    ]
+                            }
+                        ]
+                }
+            ]
+    };
 };
